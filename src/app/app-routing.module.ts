612a3f8d8a099b2ccce46c798af8902b1d9c530a
Coleman Escue <99232536+Nam3loc@users.noreply.github.com>
2023-09-26 16:06:55 -0400

    improved the homepage and added HTML and basic css to about page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,24 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
+import { HomepageComponent } from './homepage/homepage.component';
+import { AboutComponent } from './about/about.component';
+import { DogsComponent } from './dogs/dogs.component';
+import { ContactComponent } from './contact/contact.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomepageComponent},
+  {path: 'about', component: AboutComponent},
+  {path: 'dogs', component: DogsComponent},
+  {path: 'contact', component: ContactComponent},
+]
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  declarations: [],
+  imports: [
+    RouterModule.forRoot(routes)
+  ],
+  exports: [
+    RouterModule
+  ]
 })
 export class AppRoutingModule { }
