3492e3a87ee340c0579f2976c5aa389c9ac9173b
Coleman Escue <99232536+Nam3loc@users.noreply.github.com>
2023-10-05 12:07:23 -0400

    Working on the contact page

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,16 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      width: {
+        'contactWidth': '88dvw',
+        'contactHeight': '60dvw',
+        'leftWidth': '31.875%',
+        'leftHeight': '58.5%',
+        'rightWidth': '52.5%',
+        'rightHeight': '58.5%',
+      }
+    },
     colors: {
       'gold': '#FFBD59'
     },
